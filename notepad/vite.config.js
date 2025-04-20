e9ad5b1f332b2b385b9fdf04e13e4ac52d654981
wnvei <wvei.biz@gmail.com>
2025-04-20 20:16:26 +0530

    Updated vite.config.js for deployment

--- notepad/vite.config.js
+++ notepad/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  base: '/',
 })
